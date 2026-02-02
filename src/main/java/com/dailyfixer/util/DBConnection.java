59f9a0ec68c360be9c2d60454d292cdee81bd43a
Ashen Wattegedera <ashenwattegedera@gmail.com>
2026-02-02 15:15:58 +0530

    merged_product_and_store

--- src/main/java/com/dailyfixer/util/DBConnection.java
+++ src/main/java/com/dailyfixer/util/DBConnection.java
@@ -23,7 +23,7 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/dailyfixerdt"
+    private static final String URL = "jdbc:mysql://localhost:3306/dfguidestore"
             + "?useSSL=false"
             + "&serverTimezone=UTC"
             + "&allowPublicKeyRetrieval=true";
